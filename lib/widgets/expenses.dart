2786136777488c15274c53179d0a0061c17eb8bc
Your Name Here <your@email.example>
2024-09-13 17:28:04 +0530

    added appbar button to open modal

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:expense_tracker/widgets/expenses_list/expenses_list.dart';
 import 'package:expense_tracker/model/expense.dart';
 import 'package:flutter/material.dart';
@@ -12,14 +10,36 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
-
   final List<Expense> _registeredExpenses = [
-    Expense(title: 'Flutter Course', amount: 19.99, date: DateTime.now(), category: Category.work),
-    Expense(title: 'Cinema', amount: 15.69, date: DateTime.now(), category: Category.leisure)
+    Expense(
+        title: 'Flutter Course',
+        amount: 19.99,
+        date: DateTime.now(),
+        category: Category.work),
+    Expense(
+        title: 'Cinema',
+        amount: 15.69,
+        date: DateTime.now(),
+        category: Category.leisure)
   ];
+
+  void _openAddExpenseOVerlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const Center(child: Text('data')));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter expense tracker'),
+        actions: [
+          IconButton(
+            onPressed: _openAddExpenseOVerlay,
+            icon: const Icon(Icons.add),
+          )
+        ],
+      ),
       body: Column(
         children: [
           const Text('chart  '),
