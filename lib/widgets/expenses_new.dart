0e30a88da83cc1598a3fa06597c02c8056c6eaa8
Your Name Here <your@email.example>
2024-09-14 10:56:37 +0530

    date input added

--- lib/widgets/expenses_new.dart
+++ lib/widgets/expenses_new.dart
@@ -8,32 +8,84 @@ class ExpensesNew extends StatefulWidget {
 }
 
 class _ExpensesNewState extends State<ExpensesNew> {
-  
   final _titleController = TextEditingController();
+  final _amountController = TextEditingController();
 
-@override
+  void _presentDatePicker() {
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1);
+    showDatePicker(context: context, firstDate: firstDate, lastDate: now);
+  }
+
+  @override
   void dispose() {
     _titleController.dispose();
+    _amountController.dispose();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextField(
-          controller: _titleController,
-            maxLength: 50,
-            decoration: const InputDecoration(label: Text('Title'))),
-        Row(
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  print(_titleController.text);
-                },
-                child: const Text('Save Expense '))
-          ],
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        children: [
+          TextField(
+              controller: _titleController,
+              maxLength: 50,
+              decoration: const InputDecoration(label: Text('Title'))),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              Expanded(
+                child: TextField(
+                    controller: _amountController,
+                    keyboardType: TextInputType.number,
+                    maxLength: 50,
+                    decoration: const InputDecoration(
+                      prefixText: '\$ ',
+                      label: Text('Amount'),
+                    )),
+              ),
+              const SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    const Text('Selected date'),
+                    IconButton(
+                        onPressed: _presentDatePicker,
+                        icon: const Icon(Icons.calendar_month))
+                  ],
+                ),
+              )
+            ],
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    print(_titleController.text);
+                    print(_amountController.text);
+                  },
+                  child: const Text('Save Expense ')),
+              const Spacer(),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: const Text('Clear'))
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
