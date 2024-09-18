e959801eaf84fdb0ec57327900fb814f8c10e8d5
Your Name Here <your@email.example>
2024-09-18 19:10:04 +0530

    named constructor added

--- lib/model/expense.dart
+++ lib/model/expense.dart
@@ -38,6 +38,7 @@ class ExpenseBucket {
   final Category category;
   final List<Expense> expenses;
 
+ExpenseBucket.forCategory(List<Expense> allExpenses,this.category):expenses = allExpenses.where((expense) =>expense.category == category).toList(); 
   double get totalExpenses {
     double sum = 0;
 
